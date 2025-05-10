4bb697990359dafbe978d9538fda4a4c213363ed
austinalex1995 <aealex1995@gmail.com>
2025-05-10 16:22:17 -0400

    refactor: optimized JarvisTextInput

--- app/components/JarvisTextInput.tsx
+++ app/components/JarvisTextInput.tsx
@@ -1,59 +1,40 @@
 import React, { useState } from 'react';
-import { TextInput } from "react-native";
+import { StyleProp, TextInput, TextStyle } from "react-native";
 
 export interface JarvisTextInputProps {
-    value: string;
     placeholder: string;
     autoFocus?: boolean;
-    paddingLeft?: number;
-    paddingRight?: number;
-    paddingTop?: number;
-    paddingBottom?: number;
-    marginLeft?: number;
-    marginRight?: number;
-    marginTop?: number;
-    marginBottom?: number;
+    style?: StyleProp<TextStyle>;
+    onTextChange: (value:string) => void;
 }
 
 const JarvisTextInput = ({
-    value,
     placeholder,
     autoFocus = false,
-    paddingLeft = 0,
-    paddingRight = 0,
-    paddingTop = 0,
-    paddingBottom = 0,
-    marginLeft = 0,
-    marginRight = 0,
-    marginTop = 0,
-    marginBottom = 0,
+    style = {},
+    onTextChange
 } : JarvisTextInputProps) => {
     const [isSelected,setSelected] = useState(autoFocus);
+    const [currentValue,setCurrentValue] = useState("");
 
     return (
         <TextInput 
-                placeholder={placeholder}
-                value={value}
-                autoFocus={autoFocus}
-                onBlur={() => setSelected(false)}
-                onFocus={() => setSelected(true)}
-                style={[{
-                    height:50,
-                    width:300,
-                    backgroundColor:'#ffffff',
-                    borderRadius:5,
-                    borderColor: isSelected ? 'blue' : 'red',
-                    margin:5,
-                    paddingLeft: paddingLeft ? paddingLeft : 0,
-                    paddingRight: paddingRight ? paddingRight : 0,
-                    paddingTop: paddingTop ? paddingTop : 0,
-                    paddingBottom: paddingBottom ? paddingBottom : 0,
-                    marginLeft: marginLeft ? paddingLeft : 0,
-                    marginRight: marginRight ? paddingRight : 0,
-                    marginTop: marginTop ? paddingTop : 0,
-                    marginBottom: marginBottom ? paddingBottom : 0,
-                }]}>
-            </TextInput>
+            placeholder={placeholder}
+            placeholderTextColor='grey'
+            autoFocus={autoFocus}
+            onBlur={() => setSelected(false)}
+            onFocus={() => setSelected(true)}
+            onChangeText={(text) => { onTextChange(text) }}
+            style={[{
+                height:50,
+                backgroundColor:'#FFFFFF',
+                borderRadius:5,
+                textAlign: "left",
+                color: 'black',
+                paddingLeft:15
+            }, style]}>
+        </TextInput>
     )
-
 }
+
+export default JarvisTextInput;
