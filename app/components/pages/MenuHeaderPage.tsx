b56f7c3eedbb7f6410e9d2047f43bc95c218350a
austinalex1995 <aealex1995@gmail.com>
2025-05-10 16:21:36 -0400

    feat: added MenuHeaderPage component

---
+++ app/components/pages/MenuHeaderPage.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from "expo-router";
+import { StyleSheet } from "react-native";
+import { IconType } from "../IconContainer";
+import { IconButtonProps } from "../buttons/IconButton";
+import BaseHeader from "../headers/BaseHeader";
+import BasePage from "./BasePage";
+
+interface MenuHeaderPageProps {
+    backgroundColor: string;
+    title?: string;
+    rightActionIcon?: IconButtonProps;
+  }
+  
+const MenuHeaderPage: React.FC<React.PropsWithChildren<MenuHeaderPageProps>> = ({ 
+    backgroundColor,
+    title = '', 
+    rightActionIcon = undefined,
+    children
+}) => {
+    const router = useRouter();
+
+    const handleMenuButtonClicked = () => {
+        //TODO: Open Menu Drawer
+    }
+
+    return (
+        <BasePage backgroundColor={backgroundColor}>
+            <BaseHeader 
+                title={title}
+                leftActionIcon={{
+                    iconProps: {
+                        name:'menu',
+                        size:24,
+                        color:'white',
+                        type:IconType.Feather
+                    },
+                    onIconClicked: handleMenuButtonClicked
+                }}
+                rightActionIcon={rightActionIcon}
+            />
+            {children}
+        </BasePage>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+      backgroundColor: '#9cb43c',
+      height: 30,
+      paddingHorizontal: 15,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+});
+
+export default MenuHeaderPage;
